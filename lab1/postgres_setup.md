39fa3e3ce73890149e402058a3fcc16a9924e3fe
jellyboston <bostonjake2004@gmail.com>
2025-09-21 21:54:24 -0400

    feat: publisher seed, broken sanity test

--- lab1/postgres_setup.md
+++ lab1/postgres_setup.md
@@ -10,6 +10,11 @@ docker exec -i cpsc437-postgres-1 psql -U admin -d default_database -c "CREATE D
 ```
 
 Ensure user has all privileges on the database:
-```
+```bash
 docker exec -i cpsc437-postgres-1 psql -U admin -d library -c "GRANT ALL PRIVILEGES ON DATABASE library TO admin"
+```
+
+To query the database:
+```bash
+docker exec -it cpsc437-postgres-1 psql -U admin -d {db}
 ```
--- lab2/code/task2_seed.py
+++ lab2/code/task2_seed.py
@@ -7,6 +7,12 @@ from datetime import datetime, timedelta
 
 from database_actions import database_actions
 
+# Seed counts
+NUM_PUBLISHERS = 3
+NUM_AUTHORS = 20
+NUM_BOOKS = 200
+NUM_STUDENTS = 100
+NUM_BORROWS = 500
 
 def seed_database():
     """
@@ -18,8 +24,33 @@ def seed_database():
     Record 500 borrow transactions, associating books with students.
     """
     # TODO: Implement me
-    pass
+    # Publisher data
+    publisher_attr = ['publisher_name']
+    publisher_values = ['Charlie', 'Beta', 'Alpha']
+    # publisher_fields = []
+    for name in random.sample(publisher_values, NUM_PUBLISHERS):
+        # no repeats -> small sample allows for deterministic debugging
+        database_actions.execute_insert('publisher', publisher_attr, (name,))
+        # store publisher_id for sanity testing
+        # publisher_fields.append(query_id(name, publisher_fields)
 
+    # Author data
+    
+
+def query_id(name, publisher_fields):
+    with database_actions.DatabaseConnection() as cursor:
+        cursor.execute(
+            """
+            SELECT publisher_id, publisher_name
+            FROM publisher
+            WHERE publisher_name = %s
+            LIMIT 1;
+            """,
+            (name,)  # <- pass params as a tuple
+        )
+        row = cursor.fetchone()  # (publisher_id, publisher_name)
+        if row:
+            return row
 
 if __name__ == '__main__':
     seed_database()
