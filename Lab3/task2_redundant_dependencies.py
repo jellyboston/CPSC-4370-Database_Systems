eaaf455ccbfad0d43e6408f127136ac6a52d7428
jellyboston <bostonjake2004@gmail.com>
2025-10-12 15:07:15 -0400

    feat: detection of redundant dependencies - algorithm 2 in the specs

--- Lab3/task2_redundant_dependencies.py
+++ Lab3/task2_redundant_dependencies.py
@@ -7,6 +7,21 @@ def determine_redundant_dependencies(
 ) -> FunctionalDependencySet:
     """
     Determine the redundant dependencies in F
+
+    Postconditions: 
+    - Removes a->b from F to computes F' = F \ {a->b}
+    - Checks if b subset a+ under F' --> if so, determines redundant FD a->b
     """
     # TODO: implement this function (~ 6 lines)
-    pass
+    redund_fd_set = FunctionalDependencySet()
+    F_copy = FunctionalDependencySet(F) # avoids mutability on original
+    for fd in F:
+        # 1. Compute F'
+        F_prm = F_copy.remove_fd(fd)
+        # 2. Check if b subset a+ under F'
+        a, b = fd
+        a_closure = compute_closure(F_prm, a)
+        if b.issubset(a_closure):
+            # FD a->b is not redundant, so add to unique set
+            redund_fd_set.add(FunctionalDependency(a, b))
+    return redund_fd_set
