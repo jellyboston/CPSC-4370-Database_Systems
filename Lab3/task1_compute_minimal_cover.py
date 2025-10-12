94c86ad35b44f42064e15e427d2e90e2e645c583
jellyboston <bostonjake2004@gmail.com>
2025-10-12 14:36:00 -0400

    feat: driver function to compute minimal cover

--- Lab3/task1_compute_minimal_cover.py
+++ Lab3/task1_compute_minimal_cover.py
@@ -15,9 +15,12 @@ def compute_minimal_cover(F: FunctionalDependencySet) -> FunctionalDependencySet
     return: the minimal cover of F
     """
     # 1. TODO: Decompose (1 line)
+    decomposed_fds = apply_decomposition(F)
 
     # 2. TODO: Remove extraneous attributes from the left hand side (1 line)
+    unique_fds = remove_extraneous_attributes(decomposed_fds)
 
     # 3. TODO: Union property (1 line)
+    unioned_fds = apply_union(unique_fds)
 
-    return F
+    return unioned_fds
